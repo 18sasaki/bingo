4842b6a42a4eb00dd22db310172a044d5233d94a
18sasaki <18sasaki@gmail.com>
2013-03-21 06:10:24 +0900

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bingo::Application.config.secret_token = 'f6439de33fae8352b51fdf4791dfe864c6898e3f519a12ac3996442eeb814d55f2ad1f914d7952e5d9e252761cdb093659373b873404dd335abaff39e19fa646'
